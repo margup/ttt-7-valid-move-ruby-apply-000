d6feee57a8f8186f7fd5d74594b0ddf6f7198963
bright-lifecycle-9875 <mairgup@icloud.com>
2020-08-31 16:21:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
 # code your #valid_move? method here
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil 
+      return false
+    elsif (board[index] == "X")  || (board[index] =="O")
+      return false
+    else 
+      return true
+    end
+  end
+  def valid_move?(board,index)
+  if index.between?(0,8)
+    return true
+  else
+    return false
+  end
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board,index)
+  if (valid_move?(board,index)) == true && (position_taken(board, index) == false)
+    return true
+  else
+    return false
+  end
+end
