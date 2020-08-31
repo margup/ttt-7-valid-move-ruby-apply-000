8cf32696c391d10643360e933dd5f3678e2541b5
bright-lifecycle-9875 <mairgup@icloud.com>
2020-08-31 16:25:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,26 +1,26 @@
 # code your #valid_move? method here
-  def 
-   def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil 
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
-    elsif (board[index] == "X")  || (board[index] =="O")
-      return false
-    else 
+    else
       return true
     end
   end
-  def valid_move?(board,index)
-  if index.between?(0,8)
-    return true
-  else
-    return false
-  end
-end
 
-def valid_move?(board,index)
-  if (valid_move?(board,index)) == true && (position_taken(array, ind) == false)
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
+
+
 end
